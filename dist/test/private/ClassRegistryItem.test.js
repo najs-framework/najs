52672b539891d93e10932bf0e5d089c189147ae4
Tony <nhat.phan@ntworld.net>
2018-01-05 20:26:07 +0700

    Remove test in dist

--- dist/test/private/ClassRegistryItem.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("jest");
-describe('ClassRegistryItem', function () {
-    it('should work', function () { });
-});
-//# sourceMappingURL=ClassRegistryItem.test.js.map
